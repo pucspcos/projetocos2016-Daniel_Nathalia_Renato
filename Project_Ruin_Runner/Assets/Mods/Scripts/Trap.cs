d10bb703cfa05dbd29d145e9549f08cdb980cdf5
Daniel Haruo Tamura <danielhtelastico@gmail.com>
2016-11-14 20:17:24 -0200

    Versão Final.

---
+++ Project_Ruin_Runner/Assets/Mods/Scripts/Trap.cs
@@ -0,0 +1,40 @@
+﻿/**
+ * Created by Daniel Haruo Tamura
+ */
+using UnityEngine;
+
+public class Trap : MonoBehaviour {
+	ModdedGameManager moddedGameManager = null;
+
+	Animator trapAnimator;
+
+	public bool isAtivada = false;
+	public int damage = 50;
+
+	void Start () 
+	{
+		moddedGameManager = FindObjectOfType<ModdedGameManager> ();
+
+		trapAnimator = this.gameObject.GetComponent<Animator> ();
+		trapAnimator.SetBool("active", false);
+		trapAnimator.SetBool("decoy", false);
+	}
+
+	void OnTriggerEnter2D(Collider2D colisor) 
+	{
+		if (colisor.gameObject.tag.ToString() == "Player" && !isAtivada) 
+		{
+			isAtivada = true;
+
+			if (Random.Range(0, 101) > 50)
+			{
+				moddedGameManager.sufferDamage (damage);
+				trapAnimator.SetBool ("active", true);
+			} 
+			else
+			{
+				trapAnimator.SetBool("decoy", true);
+			}
+		}
+	}
+}
